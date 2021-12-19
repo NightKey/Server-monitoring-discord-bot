d37fc3f89cd7fe787c9a6d38086eb2a2eb424396
Night <davidjantho@gmail.com>
2021-12-19 20:10:37 +0100

    Added Swap status to status

--- modules/status.py
+++ modules/status.py
@@ -1,6 +1,8 @@
+from typing import Collection, Dict, Union
 from modules import bar
 from datetime import timedelta
 import psutil
+valid_fstypes = ["ntfs", "ext4", "ext3"]
 
 def get_temp() -> float:
     if not hasattr(psutil, "sensors_temperatures"):
@@ -11,9 +13,7 @@ def get_temp() -> float:
     cpu_temps = temps["coretemp"]
     return cpu_temps[0].current
 
-valid_fstypes = ["ntfs", "ext4", "ext3"]
-
-def get_disk_status():
+def get_disk_status() -> Dict[str, str]:
     disks = dict()
     partitions = psutil.disk_partitions()
     for partition in partitions:
@@ -21,30 +21,28 @@ def get_disk_status():
             disks[partition._asdict()["mountpoint"]] = psutil.disk_usage("{}".format(partition._asdict()["mountpoint"]))._asdict()
     return disks
 
-def get_pc_status():
+def get_pc_status() -> Union[Dict[str, str], Dict[str, dict], Dict[str, str]]:
     """With the help of the psutil module, scanns the PC for information about all the drives, the memory and the battery, if it has one.
     Returns disk, memory, battery in this order.
     """
     disks = get_disk_status()
-    memory = psutil.virtual_memory()._asdict()
+    memory = {"RAM":psutil.virtual_memory()._asdict(), "SWAP": psutil.swap_memory()._asdict()}
     battery = get_battery_status()
     return disks, memory, battery
 
-def get_battery_status():
+def get_battery_status() -> Dict[str, str]:
     try:
         battery = psutil.sensors_battery()._asdict()
     except:
         battery = None
     return battery
 
-def get_graphical(bar_size, in_dict=False):
+def get_graphical(bar_size, in_dict=False) -> Union[str, Dict[str, str]]:
     """Using the bar module, creates a visual representation of the system's status.
     It shows the disks' and the momory's percentage, the used and the total space, and the battery's remaning lifetime, if it's pugged, and the battery's percentage.
     """
     disks, memory, battery = get_pc_status()
     bars = bar.loading_bar("", 100, size=bar_size, show="▓", off_show="░")
-    bars.update(round(memory["percent"], 1), False)
-    memory["bar"] = bars.bar()
     if battery != None:
         bars.update(round(battery["percent"], 1), False)
         battery["bar"] = bars.bar()
@@ -60,17 +58,20 @@ def get_graphical(bar_size, in_dict=False):
         tmp = round(int(disk["used"]) / (1024 **3), 2)
         used = f"{tmp} GiB"
         if in_dict:
-            d[f"{mp.upper()} drive"]=[total, used, dbar]
+            d[f"{mp.upper()}"]=[total, used, dbar]
         else:
             string += f"{mp}: Max: {total}, used: {used}\n{dbar}\n"
-    tmp = round(int(memory["used"]) / (1024 **3), 2)
-    used = f"{tmp} GiB"
-    tmp = round(int(memory["total"]) / (1024 **3), 2)
-    _max = f"{tmp} GiB"
-    if in_dict:
-        d["Memory"]=[_max, used, memory['bar']]
-    else:
-        string += f"Max memory: {_max} / Used memory: {used}\n{memory['bar']}\n"
+    for key in ["RAM", "SWAP"]:
+        tmp = round(int(memory[key]["used"]) / (1024 **3), 2)
+        used = f"{tmp} GiB"
+        tmp = round(int(memory[key]["total"]) / (1024 **3), 2)
+        _max = f"{tmp} GiB"
+        bars.update(round(memory["RAM"]["percent"], 1), False)
+        _bar = bars.bar()
+        if in_dict:
+            d[key]=[_max, used, _bar]
+        else:
+            string += f"Max RAM memory: {_max} / Used memory: {used}\n{_bar}\n"
     if battery == None:
         if in_dict:
             d['Battery']=["Not detected"]
